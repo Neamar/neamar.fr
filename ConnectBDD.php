3f84c7822f3a3588525bb54eafb92ec911043992
Neamar <neamar@neamar.fr>
2018-01-02 15:16:09 +0100

    Select correct DB

--- ConnectBDD.php
+++ ConnectBDD.php
@@ -6,6 +6,6 @@
   $url = parse_url(getenv("DATABASE_URL"));
   //Fichier assez utile :-)
   mysql_connect($url["host"] . ":" . $url["port"], $url["user"], $url["pass"]); // Connexion à MySQL légèrement sécurisée.
-  mysql_select_db("omnilogie"); // Sélection de la base de données
+  mysql_select_db("neamar"); // Sélection de la base de données
   mysql_set_charset('latin1');
 ?>
