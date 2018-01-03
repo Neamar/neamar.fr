5fe6f9ffffc19feda5dd8fa671c9f96f20dc9224
Neamar <neamar@neamar.fr>
2018-01-03 19:18:20 +0100

    Responsive resources

--- Res/header.php
+++ Res/header.php
@@ -49,6 +49,7 @@ $keyWords='';
 <head>
 	<title><?php echo $Titre; ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="owner" content="Neamar" />
 	<meta name="author" content="Neamar" />
 	<meta name="robots" content="all" />
