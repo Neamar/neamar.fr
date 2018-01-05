24ed3a5d172d6ab1a6d7f80e783ce47079376ed4
Neamar <neamar@neamar.fr>
2018-01-05 14:36:13 +0100

    Fix timeline

--- Timeline.php
+++ Timeline.php
@@ -66,11 +66,6 @@ $Liens=array(
 
 <h1>Timeline Neamar</h1>
 
-<div style="width:20%; float:right; border:1px solid black; padding:5px;">
-<script type="text/javascript" src="//www.google.com/reader/ui/publisher-fr.js"></script>
-<script type="text/javascript" src="//www.google.com/reader/public/javascript/user/16820300681529559932/label/NeamarWorld?n=5&callback=GRC_p(%7Bc%3A%22-%22%2Ct%3A%22%5C%22NeamarWorld%5C%22%20via%20Neamar%22%2Cs%3A%22false%22%2Cn%3A%22true%22%2Cb%3A%22false%22%7D)%3Bnew%20GRC"></script>
-</div>
-
 <p>Cette page recense jour par jour les modifications faites sur l'ensemble des productions Neamar.<br />
 Le contenu de cette page ne saurait en aucun cas être exhaustif !</p>
 
@@ -92,7 +87,7 @@ ListIt($Liste);
 /**Liste complète*/
 echo '<hr />';
 echo '<h2 style="margin-top:50px;">Par projet</h2>';
-$Projets=mysql_query('SELECT DISTINCT Projet FROM _TimeLine ORDER BY ID DESC');
+$Projets=mysql_query('SELECT DISTINCT Projet FROM _TimeLine ORDER BY ID DESC') or die(mysql_error());
 while($Projet=mysql_fetch_assoc($Projets))
 {
 	echo '<h3><a href="' . $Liens[$Projet['Projet']] . '">' . $Projet['Projet']  . '</a></h3>';
@@ -100,7 +95,7 @@ while($Projet=mysql_fetch_assoc($Projets))
 SELECT DATE_FORMAT(Date, "%d/%m/%Y à %T") AS Date,Modification
 FROM _TimeLine
 WHERE Projet="' . $Projet['Projet'] . '"
-ORDER BY _TimeLine.Date DESC');
+ORDER BY _TimeLine.Date DESC') or die(mysql_error());
 
 	$Liste=array();
 	while($Event=mysql_fetch_assoc($Events))
