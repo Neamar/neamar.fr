fdaf08f2a5fd41ccd52f327c656e1b1ea56f8227
Neamar <neamar@neamar.fr>
2018-11-01 22:48:09 +0000

    Removed ads

--- Res/footer.php
+++ Res/footer.php
@@ -19,17 +19,6 @@
 </div>
 
 <div id="Footer" class="centre">
-	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-	<!-- neamar.fr Res footer -->
-	<ins class="adsbygoogle"
-	     style="display:block"
-	     data-ad-client="ca-pub-4506683949348156"
-	     data-ad-slot="9524303321"
-	     data-ad-format="auto"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
-
 	<!--Ressource sous licence Creative Commons BY-->
 	<p class="centre cliquable">
 		<img src="https://licensebuttons.net/l/by/3.0/88x31.png" alt="Licence Creative Commons : BY" class="nonflottant" onclick="document.location='http://creativecommons.org/licenses/by/3.0/deed.fr';" title="Cliquez pour plus d'informations"/>
