4632f3dd482947ae012f7280495d3324676d51db
Neamar <neamar@neamar.fr>
2019-09-27 15:47:52 +0200

    Include body

--- email.php
+++ email.php
@@ -28,6 +28,8 @@ $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
