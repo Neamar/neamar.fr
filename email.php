8ad3ccfd9f8294a9206ae0ea30c12f05231781a2
Neamar <neamar@neamar.fr>
2019-09-27 15:12:57 +0200

    Added email form

---
+++ email.php
@@ -0,0 +1,20 @@
+<?php
+require 'vendor/autoload.php'; // If you're using Composer (recommended)
+
+$email = new \SendGrid\Mail\Mail();
+$email->setFrom("test@example.com", "Example User");
+$email->setSubject("Sending with SendGrid is Fun");
+$email->addTo("neamart@gmail.com", "Example User");
+$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+$email->addContent(
+    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+);
+$sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+try {
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
+} catch (Exception $e) {
+    echo 'Caught exception: '. $e->getMessage() ."\n";
+}
