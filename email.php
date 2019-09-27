fa2eb2b54624a58b1bece098376365e805dba79b
Neamar <neamar@neamar.fr>
2019-09-27 15:34:32 +0200

    Only allow some domains

--- email.php
+++ email.php
@@ -1,20 +1,33 @@
 <?php
-require 'vendor/autoload.php'; // If you're using Composer (recommended)
+require 'vendor/autoload.php';
+
+$emails = array(
+  "neamar.fr" => "contact@neamar.fr",
+  "omnilogie.fr" => "contact@neamar.fr",
+  "cruciverbe.fr" => "contact@neamar.fr",
+  "choltraiteur.fr" => "contact@choltraiteur.fr",
+  "endonymous" => "contact@endonymous.fr"
+);
+$allowedDomains = array_keys($emails);
+
+$domain = $_SERVER['HTTP_ORIGIN']
+echo $_SERVER['HTTP_ORIGIN'];
+
+if(!in_array($_SERVER['HTTP_ORIGIN'], $allowedDomains)) {
+  echo "Invalid domain.";
+  die();
+}
+
 
 $email = new \SendGrid\Mail\Mail();
-$email->setFrom("test@example.com", "Example User");
-$email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("neamart@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
+$email->setFrom($emails[$domain], $domain);
+$email->setSubject($_POST['_subject']);
+$email->addTo($_POST['_replyto']);
+$email->addContent("text/plain", $_POST['message']);
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
     print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
