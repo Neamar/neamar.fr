777ff4b8518f225f3a610a794417c1a7aaf29f2c
Neamar <neamar@neamar.fr>
2021-06-21 10:21:00 +0200

    Added reference to main site

--- Res/1001Blagues/index.php
+++ Res/1001Blagues/index.php
@@ -4,8 +4,7 @@ $Box = array("Auteur" => "Neamar","Date" => "2007-2008", "But" =>"Un peu d'humou
 include('../header.php');
 ?>
 <h1>1001 blagues</h1>
-<p class="erreur">Attention. Certaines des blagues présentées ici peuvent choquer les plus jeunes visiteurs.</p>
-<p class="erreur">La mise en forme, la qualité, les fautes d'orthographe de ce texte ne sont pas en accord avec le reste du contenu de ce site. Cette page est présentée au titre d'archive, sans autre prétention.</p>
+<p class="erreur">Retrouvez toutes ces blagues et bien d'autres encore dans une présentation plus claire sur le site <a href="https://jairi.fr">J'ai ri !</a>.</p>
 <ol>
 
 <?php
