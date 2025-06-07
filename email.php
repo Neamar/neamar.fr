c1a9f30edc9aca55c5e31765d3bb6f3bf0f80d34
Neamar <neamar@neamar.fr>
2025-06-07 19:17:33 +0200

    Use PHPMailer

--- email.php
+++ email.php
@@ -1,13 +1,15 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 require 'vendor/autoload.php';
-require_once "Mail.php";
 
 // Support for CORS
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Accept, Origin");
 header("Access-Control-Allow-Origin: *");
 
-$emails = array("contact@neamar.fr", "neamar@neamar.fr", "contact@choltraiteur.fr", "contact@endonymous.fr", "contact@1001fenetres.com", "contact@cycliques.fr");
+$emails = array("contact@neamar.fr", "neamar@neamar.fr", "contact@choltraiteur.fr", "contact@endonymous.fr", "contact@1001fenetres.com");
 
 if(!in_array($_POST['_to'], $emails)) {
   http_response_code(400);
@@ -39,32 +41,35 @@ $smtpPass = getenv('SMTP_PASS');
 $from = 'contact@neamar.fr';
 $to = $_POST['_replyto'];
 $subject = empty($_POST['_subject']) ? $_POST['_subject'] : 'Prise de contact';
+$body = $_POST['message'];
 
 
-$smtp = Mail::factory('smtp', [
-  'host'     => $smtpHost,
-  'auth'     => true,
-  'username' => $smtpUser,
-  'password' => $smtpPass,
-  'port'     => 465,
-  'debug'    => false
-]);
+$mailer = new PHPMailer(true);
 
-// Set up SMTP parameters
-$headers = [
-  'From'    => $from,
-  'To'      => $to,
-  'Subject' => $subject
-];
-$mail = $smtp->send($to, $headers, $body);
+try {
+  //Server settings
+  $mailer->isSMTP();
+  $mailer->Host       = $smtpHost;
+  $mailer->SMTPAuth   = true;
+  $mailer->Username   = $smtpUser;
+  $mailer->Password   = $smtpPass;
+  $mailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+  $mailer->Port       = 465;
 
-// Check for errors
-if (PEAR::isError($mail)) {
+  //Recipients
+  $mailer->setFrom($from);
+  $mailer->addAddress($_POST['_to']);
+  $mailer->addReplyTo($to);
+
+  //Content
+  $mailer->Subject = $subject;
+  $mailer->Body    = $body;
+
+  $mailer->send();
+  echo "Merci, votre message a bien été envoyé.";
+} catch (Exception $e) {
     http_response_code(500);
     echo "Impossible d'envoyer votre message. Merci de contacter directement neamar@neamar.fr";
-}
-else {
-  echo "Merci, votre message a bien été envoyé.";
 }
 
 // Keep logs
